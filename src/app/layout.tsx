dadd1bce041fee5d436071e98722138189ea93ca
newwangzhicheng <newwangzhicheng@hotmail.com>
2025-03-11 00:00:49 +0800

    add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,9 +16,30 @@ const geistMono = localFont({
 })
 
 export const metadata: Metadata = {
-  title: 'Insta J',
-  description:
-    'Instagram image and video downloader. Support post, reel, igtv...'
+  title: 'Insta J | Instagram Image and Video Downloader',
+  description: 'Instagram image and video downloader. Support post, reel, igtv...',
+  keywords: ['instagram', 'downloader', 'image', 'video', 'download', 'post', 'reel', 'igtv'],
+  authors: [{ name: 'Insta J', url: 'https://github.com/newwangzhicheng/instagram-downloader' }],
+  creator: 'Insta J',
+  publisher: 'Insta J',
+  robots: 'index, follow',
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://insta-j.vercel.app/',
+    title: 'Insta J | Instagram Image and Video Downloader',
+    description: 'Instagram image and video downloader. Support post, reel, igtv...',
+    siteName: 'Insta J'
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Insta J | Instagram Image and Video Downloader',
+    description: 'Instagram image and video downloader. Support post, reel, igtv...',
+    creator: '@InstaJ'
+  },
+  icons: {
+    icon: '/favicon.ico'
+  }
 }
 
 export default function RootLayout({
